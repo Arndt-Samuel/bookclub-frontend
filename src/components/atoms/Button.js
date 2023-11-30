15f1f85e790bc388df0c2384e9b24a3a774a793d
Samuel Arndt <samuelarndt@hotmail.com>
2023-11-30 18:32:04 -0300

    feat: finish login screen UI

---
+++ src/components/atoms/Button.js
@@ -0,0 +1,9 @@
+import { Button as ChakraButton } from '@chakra-ui/react'
+
+export const Button = ({ children, ...props }) => (
+    <ChakraButton fontWeight='bold' fontSize='16px' borderRadius='16px' h='56px' _hover={{
+      bg: 'brand.primary'
+    }} bg='brand.primary' {...props}>
+        {children}
+    </ChakraButton>
+)
