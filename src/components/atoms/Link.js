9c9929e6fe2ac427c22ffa3fdb34fc943bece687
Samuel Arndt <samuelarndt@hotmail.com>
2023-12-01 15:31:56 -0300

    feat: implement yup and formik at login form

--- src/components/atoms/Link.js
+++ src/components/atoms/Link.js
@@ -1,13 +1,13 @@
 import { Link as ChakraLink, Flex } from '@chakra-ui/react'
 
-export const Link = ({ children, props }) => (
+export const Link = ({ children, ...props }) => (
     <ChakraLink fontSize='14px' color='brand.darkGrey' {...props}>
         {children}
     </ChakraLink>
 )
 
 Link.Action = ({ text, actionText, ...props }) => (
-    <Flex w='100%' alignItems='center' justifyContent='center'>
+    <Flex flexDir={['column', 'row']} w='100%' alignItems='center' justifyContent='center'>
         <ChakraLink mr='4px' fontSize='16px' color='brand.darkGrey' {...props}>
             {text}
         </ChakraLink>
--- src/components/molecules/Input.js
+++ src/components/molecules/Input.js
@@ -1,18 +1,27 @@
 import { useState } from 'react'
+import { Text } from 'components/atoms'
 import { Input as ChakraInput, InputGroup, InputRightElement, Button } from '@chakra-ui/react'
 import { ViewIcon, ViewOffIcon } from '@chakra-ui/icons'
 
 export const Input = (props) => (
-    <ChakraInput h='56px' fontSize='16px' focusBorderColor='brand.primary' {...props}/>
+  <>
+      <ChakraInput h='56px' fontSize='16px' focusBorderColor='brand.primary' {...props}/>
+      {props.error && <Text color='red'>{props.error}</Text>}
+  </>
 )
 
-Input.Password = (props) => {
+Input.Password = ({ value, onChange, id, name, ...props }) => {
   const [show, setShow] = useState(false)
   const handleClick = () => setShow(!show)
 
   return (
+        <>
         <InputGroup display='flex' alignItems='center' justifyContent='center' fontSize='16px'h='56px' size='md' {...props}>
         <Input
+          id={id}
+          name={name}
+          onChange={onChange}
+          value={value}
           focusBorderColor='brand.primary'
           pr='4.5rem'
           type={show ? 'text' : 'password'}
@@ -24,6 +33,8 @@ Input.Password = (props) => {
           </Button>
         </InputRightElement>
       </InputGroup>
+      {props.error && <Text color='red'>{props.error}</Text>}
+      </>
   )
 }
 
