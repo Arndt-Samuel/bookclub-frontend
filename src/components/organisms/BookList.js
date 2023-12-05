659a8697a85e63802a921c82aeeb12aef806b31b
Samuel Arndt <samuelarndt@hotmail.com>
2023-12-05 17:42:49 -0300

    feat: category list

--- src/components/organisms/BookList.js
+++ src/components/organisms/BookList.js
@@ -1,13 +1,18 @@
 import { Flex } from '@chakra-ui/react'
 import { BookCard } from 'components/molecules'
 import { Text } from 'components/atoms'
-export const BookList = ({ data, error, isLoading }) => {
+import { useQuery } from 'react-query'
+import { getHighlightedBooks } from 'services/api/requests'
+
+export const BookList = () => {
+  const { data } = useQuery('highlighted', getHighlightedBooks)
+
   return (
     <Flex flexDir='column' mt="48px" paddingX={['24px', '48px', '80px', '112px']}>
       <Text.ScreenTitle>Destaques</Text.ScreenTitle>
       <Flex mt='24px' flexDir="row">
-        {data &&
-          data?.map((item) => <BookCard key={`book_${item.id}`} {...item} />)}
+        {data?.data &&
+          data?.data.map((item) => <BookCard key={`book_${item.id}`} {...item} />)}
       </Flex>
     </Flex>
   )
